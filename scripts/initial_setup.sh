f629ee7868fa8a3a813320bbd0322981eeb4edd8
John Cipriano <johnmikecip@gmail.com>
2019-02-18 20:35:01 -0800

    update initial_setup for vim-plug

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -142,8 +142,9 @@ vagrant plugin install vagrant-gatling-rsync
 
 
 # Install Vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -s ./commands.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim -s ./commands.vim
 
 
 # Install fonts
