ce0bf30a4b8195ff1a122078604b22f5d6ef63f3
John Cipriano <johnmikecip@gmail.com>
2025-01-23 00:44:13 -0800

    wezterm.lua: use smyck color scheme

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -8,7 +8,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Material Darker'
+-- config.color_scheme = 'Mariana'
+-- config.color_scheme = 'Material (base16)'
+-- config.color_scheme = 'Material Darker (base16)'
+-- config.color_scheme = 'Seti UI (base16)'
+config.color_scheme = 'Smyck (Gogh)'
 
 config.keys = {
   {
