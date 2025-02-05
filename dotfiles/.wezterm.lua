0f84e4432bef483e305dc679018fd2549ce7298c
John Cipriano <johnmikecip@gmail.com>
2025-02-05 11:56:21 -0800

    clean up wezterm config

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -1,18 +1,15 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 local act = wezterm.action
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+config.color_scheme = 'Smyck (Gogh)'
 -- config.color_scheme = 'Mariana'
 -- config.color_scheme = 'Material (base16)'
 -- config.color_scheme = 'Material Darker (base16)'
 -- config.color_scheme = 'Seti UI (base16)'
-config.color_scheme = 'Smyck (Gogh)'
 
 config.keys = {
   {
@@ -22,5 +19,4 @@ config.keys = {
   },
 }
 
--- and finally, return the configuration to wezterm
 return config
