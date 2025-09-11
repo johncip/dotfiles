d8f73ba92d82761d5c58cf3e727457a6ba6a8a23
John Cipriano <johnmikecip@gmail.com>
2025-09-11 14:27:03 -0700

    wezterm: change color scheme

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -4,12 +4,14 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 
 
+config.font_size = 14.0
+
 -- For example, changing the color scheme:
-config.color_scheme = 'Smyck (Gogh)'
+-- config.color_scheme = 'Smyck (Gogh)'
 -- config.color_scheme = 'Mariana'
 -- config.color_scheme = 'Material (base16)'
 -- config.color_scheme = 'Material Darker (base16)'
--- config.color_scheme = 'Seti UI (base16)'
+config.color_scheme = 'Seti UI (base16)'
 
 
 -- Remove padding between text and window edge
