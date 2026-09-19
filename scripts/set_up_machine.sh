0d3baee01bab40e016f8deb6b586cf2f527e7d8f
John Cipriano <johnmikecip@gmail.com>
2026-09-18 22:56:59 -0700

    update asdf line in set up script

--- scripts/set_up_machine.sh
+++ scripts/set_up_machine.sh
@@ -132,7 +132,7 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin add python https://github.com/danhper/asdf-python.git
 asdf install nodejs latest
-asdf global nodejs latest
+asdf set -u nodejs latest
 
 
 # Install pure prompt
